965df37a7bd96096d74860f679ab31c280e8032c
OmarAbdElRaouf1 <omahussien@std.mans.edu.eg>
2025-09-12 12:34:21 +0300

    finish PayPal Payment

--- lib/features/checkout/domain/entities/payment_entity.dart
+++ lib/features/checkout/domain/entities/payment_entity.dart
@@ -1,1 +1,11 @@
+class paymentEntity {
+  final String id;
+  final String method;
+  final double amount;
 
+  paymentEntity({
+    required this.id,
+    required this.method,
+    required this.amount,
+  });
+}
